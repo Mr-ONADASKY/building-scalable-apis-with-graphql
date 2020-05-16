9fc97da140b08438cec40b0553c08e3dbbecb127
Nick Vanden Eynde <nvde1960@gmail.com>
2020-05-17 00:58:30 +0200

    setup express server

--- name-contests/src/index.ts
+++ name-contests/src/index.ts
@@ -1,15 +1,25 @@
-import { graphql } from 'graphql';
 import nodeEnv from './util';
 import { ncSchema } from './schema';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: ncSchema,
+    graphiql: true,
+  }),
+);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
 const init = async () => {
   console.log(`Running in ${nodeEnv} mode...`);
-
-  const query = process.argv[2];
-
-  const response = await graphql(ncSchema, query);
-
-  console.log(response);
 };
 
 init();
